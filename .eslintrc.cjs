867b582e9f1d7baea21da95cadcae39a4cded7db
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-11-16 22:09:56 +0900

    전체적인 린트 적용

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,33 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
-  "root": true,
-  "extends": [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-prettier"
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier',
   ],
-  "env": {
-    "vue/setup-compiler-macros": true
-  }
-}
+  env: {
+    'vue/setup-compiler-macros': true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+        endOfLine: 'auto', // 한줄 추가
+      },
+    ],
+  },
+};
