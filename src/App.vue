75d3cb0ab42b23e9cf0fa473d4b924ded6872764
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-11-16 22:23:50 +0900

    기본 UI

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
-<script setup></script>
+<script setup>
+import TheHeader from "@/layouts/TheHeader";
+import TheView from "@/layouts/TheView";
+</script>
 
 <template>
-  <main></main>
+  <the-header></the-header>
+  <the-view></the-view>
 </template>
 
 <style></style>
