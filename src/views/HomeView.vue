5fdb3dd501b3b5a9474fa476d1a8fc5c73cf6f9c
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-11-16 23:28:04 +0900

    route name

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h2>Home View</h2>
-    <p>{{ $route }}</p>
+    <p>{{ $route.name }}</p>
     <button class="btn btn-primary" @click="goAboutPage">About으로 이동</button>
   </div>
 </template>
