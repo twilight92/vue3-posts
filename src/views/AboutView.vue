6d6e6f553c353f139adfe9811132ce227130d4db
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-11-16 23:22:41 +0900

    라우터 적용

---
+++ src/views/AboutView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h2>About View</h2>
+    <p>{{ $route }}</p>
+  </div>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>Home View</h2>
+    <p>{{ $route }}</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
