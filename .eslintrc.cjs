99df7bc24024f3b2ac34257d696a7e04c9d8a49e
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-11-17 09:44:04 +0900

    린트 적용

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,32 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution');
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
   extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier',
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-prettier",
   ],
   env: {
-    'vue/setup-compiler-macros': true,
+    "vue/setup-compiler-macros": true,
   },
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {
-    'prettier/prettier': [
-      'error',
+    "prettier/prettier": [
+      "error",
       {
         singleQuote: true,
         semi: true,
         tabWidth: 2,
-        trailingComma: 'all',
+        trailingComma: "all",
         printWidth: 80,
         bracketSpacing: true,
-        arrowParens: 'avoid',
-        endOfLine: 'auto', // 한줄 추가
+        arrowParens: "avoid",
+        endOfLine: "auto", // 한줄 추가
       },
     ],
   },
