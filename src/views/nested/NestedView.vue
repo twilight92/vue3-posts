010f4584617e2b31df214b7c5cc9314c6db599ba
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2025-01-04 16:51:11 +0900

    router.push와 router.replace 차이 알아보기

--- src/views/nested/NestedView.vue
+++ src/views/nested/NestedView.vue
@@ -2,12 +2,18 @@
   <div>
     <ul class="nav nav-pills">
       <li class="nav-item">
-        <router-link class="nav-link" active-class="active" to="/nested/one"
+        <router-link
+          class="nav-link"
+          active-class="active"
+          :to="{ name: 'NestedOne', replace: true }"
           >Nested One</router-link
         >
       </li>
       <li class="nav-item">
-        <router-link class="nav-link" active-class="active" to="/nested/two"
+        <router-link
+          class="nav-link"
+          active-class="active"
+          :to="{ name: 'NestedTwo', replace: true }"
           >Nested two</router-link
         >
       </li>
