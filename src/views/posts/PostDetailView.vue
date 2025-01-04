7fe84044ab111ac82c2cecd4c32a361db2b86739
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2025-01-04 19:26:22 +0900

    페이지 컴포넌트에 Props 전달

--- src/views/posts/PostDetailView.vue
+++ src/views/posts/PostDetailView.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <h2>제목</h2>
-    <p>내용</p>
-    <p class="text-muted">2020-01-01</p>
+    <h2>{{ form.title }}</h2>
+    <p>{{ form.content }}</p>
+    <p class="text-muted">{{ form.createdAt }}</p>
     <hr class="my-4" />
     <div class="row g-2">
       <div class="col-auto">
@@ -29,11 +29,19 @@
 
 <script setup>
 import { useRoute, useRouter } from "vue-router";
+import { getPostById } from "@/api/posts";
+import { ref } from "vue";
 
 const route = useRoute();
 const router = useRouter();
 const id = route.params.id;
+const form = ref({});
 
+const fetchPost = () => {
+  const data = getPostById(id);
+  form.value = { ...data };
+};
+fetchPost();
 const goListPage = () => {
   router.push({
     name: "PostList",
