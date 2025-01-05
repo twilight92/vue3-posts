17a3bb8bad6c2c663eb40934f5f4cc129104cce2
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2025-01-05 16:21:20 +0900

    상세(어떠한 데이터가 올 줄 모르기 때문에 전체 data를 넣지 않고 각각 넣어주도록 변경)

--- src/views/posts/PostDetailView.vue
+++ src/views/posts/PostDetailView.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <h2>{{ form.title }}</h2>
-    <p>{{ form.content }}</p>
-    <p class="text-muted">{{ form.createdAt }}</p>
+    <h2>{{ post.title }}</h2>
+    <p>{{ post.content }}</p>
+    <p class="text-muted">{{ post.createdAt }}</p>
     <hr class="my-4" />
     <div class="row g-2">
       <div class="col-auto">
@@ -38,11 +38,16 @@ const props = defineProps({
 
 const router = useRouter();
 // const id = route.params.id;
-const form = ref({});
+const post = ref({});
 
-const fetchPost = () => {
-  const data = getPostById(props.id);
-  form.value = { ...data };
+const fetchPost = async () => {
+  const { data } = await getPostById(props.id);
+  setPost(data);
+};
+const setPost = ({ title, content, createdAt }) => {
+  post.value.title = title;
+  post.value.content = content;
+  post.value.createdAt = createdAt;
 };
 fetchPost();
 const goListPage = () => {
