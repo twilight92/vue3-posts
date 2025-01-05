9170e7335e3b5571fc83c0c5f1c3c22cc51e11ea
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2025-01-05 21:48:56 +0900

    axios - create 함수를 이용해 baseURL 적용

--- src/api/index.js
+++ src/api/index.js
@@ -4,3 +4,5 @@ function create(baseURL, options) {
   const instance = axios.create(Object.assign({ baseURL }, options));
   return instance;
 }
+
+export const posts = create("http://localhost:5000/posts/");
