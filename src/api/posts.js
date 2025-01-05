04e5b9ac8003c3f793f8b3673aadb2712b16147a
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2025-01-05 16:03:14 +0900

    axios 적용

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 // axios
 const posts = [
   { id: 1, title: "제목1", contents: "내용1", createdAt: "2020-01-01" },
@@ -8,9 +10,21 @@ const posts = [
 ];
 
 export function getPosts() {
-  return posts;
+  return axios.get("http://localhost:5000/posts");
 }
 
 export function getPostById(id) {
-  return posts.find((item) => item.id === id);
+  return axios.get(`http://localhost:5000/posts/${id}`);
+}
+
+export function createPost(data) {
+  return axios.post("http://localhost:5000/posts", data);
+}
+
+export function updatePost(id, data) {
+  return axios.put(`http://localhost:5000/posts/${id}`, data);
+}
+
+export function deletePost(id) {
+  return axios.delete(`http://localhost:5000/posts/${id}`);
 }
