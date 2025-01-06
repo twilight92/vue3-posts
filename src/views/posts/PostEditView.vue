3cc8aafbc993eb8374d779c24698c83abea5dfce
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2025-01-06 21:00:57 +0900

    공통 컴포넌트 분리

--- src/views/posts/PostEditView.vue
+++ src/views/posts/PostEditView.vue
@@ -2,36 +2,24 @@
   <div>
     <h2>게시글 수정</h2>
     <hr class="my-4" />
-    <form @submit.prevent="edit">
-      <div class="mb-3">
-        <label for="title" class="form-label">제목</label>
-        <input
-          v-model="form.title"
-          type="text"
-          class="form-control"
-          id="title"
-        />
-      </div>
-      <div class="mb-3">
-        <label for="content" class="form-label">내용</label>
-        <textarea
-          v-model="form.content"
-          class="form-control"
-          id="content"
-          rows="3"
-        ></textarea>
-      </div>
-      <div class="pt-4">
-        <button
-          type="button"
-          class="btn btn-outline-danger me-2"
-          @click="goDetailPage"
-        >
-          취소
-        </button>
-        <button class="btn btn-primary">수정</button>
-      </div>
-    </form>
+    <post-form
+      v-model:title="form.title"
+      v-model:content="form.content"
+      @submit.prevent="edit"
+    >
+      <template #actions>
+        <div class="pt-4">
+          <button
+            type="button"
+            class="btn btn-outline-danger me-2"
+            @click="goDetailPage"
+          >
+            취소
+          </button>
+          <button class="btn btn-primary">수정</button>
+        </div>
+      </template>
+    </post-form>
   </div>
 </template>
 
@@ -39,6 +27,7 @@
 import { ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import { getPostById, updatePost } from "@/api/posts";
+import PostForm from "@/components/posts/PostForm.vue";
 
 const route = useRoute();
 const router = useRouter();
