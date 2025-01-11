52d4f36bdc4de59093ec4a84ed739d373535d9a1
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2025-01-11 22:49:52 +0900

    alert 공통 컴포넌트 적용

--- src/views/posts/PostEditView.vue
+++ src/views/posts/PostEditView.vue
@@ -18,6 +18,7 @@
         <button class="btn btn-primary">수정</button>
       </template>
     </post-form>
+    <app-alert :show="showAlert" :message="alertMessage" :type="alertType" />
   </div>
 </template>
 
@@ -26,6 +27,7 @@ import { ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import { getPostById, updatePost } from "@/api/posts";
 import PostForm from "@/components/posts/PostForm.vue";
+import AppAlert from "@/components/AppAlert.vue";
 
 const route = useRoute();
 const router = useRouter();
@@ -59,10 +61,25 @@ const goDetailPage = () => {
 const edit = async () => {
   try {
     await updatePost(id, { ...form.value });
-    router.push({ name: "PostDetail", params: { id } });
+    // router.push({ name: "PostDetail", params: { id } });
+    vAlert("수정이 완료되었습니다", "success");
   } catch (error) {
     console.error(error);
+    vAlert("네트워크 오류");
   }
+};
+
+// alert
+const showAlert = ref(false);
+const alertMessage = ref("false");
+const alertType = ref("");
+const vAlert = (message, type = "error") => {
+  showAlert.value = true;
+  alertMessage.value = message;
+  alertType.value = type;
+  setTimeout(() => {
+    showAlert.value = false;
+  }, 2000);
 };
 </script>
 
