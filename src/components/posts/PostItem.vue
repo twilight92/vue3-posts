afb657abaf8be916b0d53b110cf657a22375238d
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2025-01-12 18:08:37 +0900

    dayjs(날짜 포맷팅 라이브러리) 적용

--- src/components/posts/PostItem.vue
+++ src/components/posts/PostItem.vue
@@ -2,7 +2,7 @@
   <app-card>
     <h5 class="card-title">{{ title }}</h5>
     <p class="card-text">{{ content }}</p>
-    <p class="text-muted">{{ createdAt }}</p>
+    <p class="text-muted">{{ createdDate }}</p>
     <template #footer>
       <div class="d-flex flex-row-reverse">
         <button class="btn p-0" @click.stop="$emit('modal')">
@@ -14,7 +14,9 @@
 </template>
 
 <script setup>
-defineProps({
+import { inject, computed } from "vue";
+
+const props = defineProps({
   title: {
     type: String,
     required: true,
@@ -28,6 +30,10 @@ defineProps({
   },
 });
 defineEmits("modal");
+const dayjs = inject("dayjs");
+const createdDate = computed(() =>
+  dayjs(props.greatedAt).format("YYYY.MM.DD HH:mm:ss")
+);
 </script>
 
 <style lang="scss" scoped></style>
