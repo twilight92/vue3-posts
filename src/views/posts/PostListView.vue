65f19935647cb71eabfd4a1c0b2eaac8520ffd62
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2025-01-12 11:21:19 +0900

    PostModal 컴포넌트화

--- src/views/posts/PostListView.vue
+++ src/views/posts/PostListView.vue
@@ -6,6 +6,12 @@
       v-model:title="params.title_like"
       v-model:limit="params._limit"
     />
+    <post-modal
+      v-model="show"
+      :title="modalTitle"
+      :content="modalContent"
+      :created-at="modalCreatedAt"
+    />
     <hr class="my-4" />
     <app-grid :items="posts">
       <template v-slot="{ item }">
@@ -23,24 +29,6 @@
       :page-count="pageCount"
       @page="(page) => (params._page = page)"
     />
-    <app-modal :show="show" title="게시글" @close="closeModal">
-      <template #default>
-        <div class="row g-3">
-          <div class="col-3 text-muted">제목</div>
-          <div class="col-9">{{ modalTitle }}</div>
-          <div class="col-3 text-muted">내용</div>
-          <div class="col-9">{{ modalContent }}</div>
-
-          <div class="col-3 text-muted">등록일</div>
-          <div class="col-9">{{ modalCreatedAt }}</div>
-        </div>
-      </template>
-      <template #actions>
-        <button type="button" class="btn btn-secondary" @click="closeModal">
-          닫기
-        </button>
-      </template>
-    </app-modal>
     <hr class="my-5" />
     <app-card>
       <post-detail-view :id="1"></post-detail-view>
@@ -52,7 +40,7 @@
 import PostItem from "@/components/posts/PostItem.vue";
 import PostDetailView from "@/views/posts/PostDetailView.vue";
 import PostFilter from "@/views/posts/PostFilter.vue";
-import AppModal from "@/components/AppModal.vue";
+import PostModal from "@/views/posts/PostModal.vue";
 import AppPagination from "@/components/AppPagination.vue";
 import AppCard from "@/components/AppCard.vue";
 import AppGrid from "@/components/AppGrid.vue";
@@ -105,9 +93,6 @@ const openModal = ({ title, content, createdAt }) => {
   modalTitle.value = title;
   modalContent.value = content;
   modalCreatedAt.value = createdAt;
-};
-const closeModal = () => {
-  show.value = false;
 };
 </script>
 
