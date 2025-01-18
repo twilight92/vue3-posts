4c089959b936fdd3b56d542299934e7a45876e6c
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2025-01-18 18:10:29 +0900

    router-link를 이용한 루트 이동

--- src/layouts/TheHeader.vue
+++ src/layouts/TheHeader.vue
@@ -2,7 +2,7 @@
   <header>
     <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">GYM CODING</a>
+        <router-link class="navbar-brand" to="/">Vue3</router-link>
         <button
           class="navbar-toggler"
           type="button"
