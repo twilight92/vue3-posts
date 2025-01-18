68f101ab35bbe59e7b1e4faee328834bb9f8c395
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2025-01-18 16:11:04 +0900

    사용하지 않는 코드 제거

--- src/main.js
+++ src/main.js
@@ -5,20 +5,10 @@ import App from "./App.vue";
 import router from "@/router";
 import globalDirectives from "./plugins/global-directives";
 import dayjs from "./plugins/dayjs";
-// import globalComponents from "./plugins/global-components";
-// import focus from "@/directives/focus";
 
 const app = createApp(App);
-// app.directive("focus", focus);
-// app.use(globalComponents);
 app.use(globalDirectives);
 app.use(dayjs);
 app.use(router);
 app.mount("#app");
 import "bootstrap/dist/js/bootstrap.js";
-
-// console.log("MODE", import.meta.env.MODE);
-// console.log("BASE_URL", import.meta.env.BASE_URL);
-// console.log("PROD", import.meta.env.PROD);
-// console.log("DEV", import.meta.env.DEV);
-// console.log("VITE_APP_API_URL", import.meta.env.VITE_APP_API_URL);
