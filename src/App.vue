53da8c88d2d2a62f67f23b954a43921fcf956951
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2025-01-18 16:41:19 +0900

    alert Store 만들기

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <script setup>
 import TheHeader from "@/layouts/TheHeader.vue";
 import TheView from "@/layouts/TheView.vue";
-import { useAlert } from "./composables/alert";
-const { alerts } = useAlert();
 </script>
 
 <template>
   <the-header></the-header>
   <the-view></the-view>
-  <app-alert :items="alerts" />
+  <app-alert />
 </template>
 
 <style></style>
